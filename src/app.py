0aa9dea3ef9d115cf2b9376514a05cda75826ebc
alimelus <66154377+alimelus@users.noreply.github.com>
2023-10-12 20:32:03 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask import Flask, render_template
-app = Flask(__name__ )
+app = Flask( __name__ )
 def hello_world():
     return render_template('index.html', date=datetime.now())
 def alive():
