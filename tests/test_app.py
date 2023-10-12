6916050b12ac41cd39b4b81bba9166c0001fa7a3
alimelus <66154377+alimelus@users.noreply.github.com>
2023-10-12 20:26:53 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,11 @@
-from app import app
+from app import hello_world, alive
 from datetime import datetime
 
 def test_hello_world():
-    assert hello_world() == "Hello, world!"
+    response = hello_world()
+    assert 'Hello, world!' in response  # Adjust the assertion based on your expected template content
 
 def test_alive():
-    assert alive() == "Hello, world!"
+    response = alive()
+    assert response == "yes"
+
