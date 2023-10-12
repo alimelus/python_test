e1000984f524cb8139999bb658161af34017e415
alimelus <66154377+alimelus@users.noreply.github.com>
2023-10-12 17:56:13 +0530

    Create setup.py

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+  name= 'helloworld-python-flask',
+  version= '1.0.3171',
+  packages=[],
+  url='',
+  license='',
+  author='S0811512',
+  author_email='',
+  description='',
+  install_requires=['flask,'uwsgi'])
