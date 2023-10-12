1749d8d07467322f87208ff8d120c4fc526d9680
alimelus <66154377+alimelus@users.noreply.github.com>
2023-10-12 20:45:55 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 from flask import Flask, render_template
-app = Flask( __name__ )
+app = Flask(__name__)
+@app.route('/')
 def hello_world():
     return render_template('index.html', date=datetime.now())
+@app.route('/health')
 def alive():
     return "yes"
 if __name__  == '__main__':
