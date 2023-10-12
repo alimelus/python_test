986e8f77dacc905ec179fece92f876fff84545af
alimelus <66154377+alimelus@users.noreply.github.com>
2023-10-12 21:24:47 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,20 +1,4 @@
-from app.my_app import app
-import unittest
+from app import hello_world
+def test_hello_world():
+    assert hello_world() == "Hello, world!"
 
-class TestMyApp(unittest.TestCase):
-
-    def setUp(self):
-        self.app = app.test_client()
-
-    def test_hello_world(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b"Hello, World!")
-
-    def test_alive(self):
-        response = self.app.get('/health')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b"yes")
-
-if __name__ == '__main__':
-    unittest.main()
