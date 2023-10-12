84b7bf3298d48ddb181726345802ada6d0fb4f09
alimelus <66154377+alimelus@users.noreply.github.com>
2023-10-12 18:18:33 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 from flask import flask, render_template
-app = Flask(_name_)
+app = Flask(__name__ )
 def hello_world():
     return render_template('index.html', date=datetime.now())
 def alive():
     return "yes"
-if _name_ == '_main_':
+if __name__  == '__main__':
     app.run()
