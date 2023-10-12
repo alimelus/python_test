917b4072da28a8f00e85d95771ef2ee1416aa617
alimelus <66154377+alimelus@users.noreply.github.com>
2023-10-12 19:31:07 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,12 @@
-from app import index
+from app import app
+from datetime import datetime
 
+def test_hello_world():
+    client = app.test_client()
+    response = client.get('/')
+    assert b'Hello, world!' in response.data
 
-def test_index():
-    assert index() == "Hello, world!"
+def test_alive():
+    client = app.test_client()
+    response = client.get('/alive')
+    assert response.data == b'yes'
