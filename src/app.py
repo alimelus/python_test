bbc80b6a4616e1ee72488e631b4fe3cb3449bced
alimelus <66154377+alimelus@users.noreply.github.com>
2023-10-12 17:54:12 +0530

    Create app.py

---
+++ src/app.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+from flask import flask, render_template
+app = Flask(_name_)
+def hello_world();
+return render_template('index.html', date=datetime.now())
+def alive();
+return "yes"
+if _name_ == '_main_'
+app.run()
