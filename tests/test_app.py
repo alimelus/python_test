7e7e9c25e965e482e94ea447f3f4d26d9a829ddb
alimelus <66154377+alimelus@users.noreply.github.com>
2023-10-12 21:02:17 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,9 @@
-import unittest
+# my_flask_app/tests/test_my_app.py
 
-class TestApp(unittest.TestCase):
+import unittest
+from my_app import app
+
+class TestMyApp(unittest.TestCase):
 
     def setUp(self):
         self.app = app.test_client()
@@ -8,13 +11,12 @@ class TestApp(unittest.TestCase):
     def test_hello_world(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Hello, world!', response.data)
+        self.assertIn(b"Hello, World!", response.data)
 
     def test_alive(self):
         response = self.app.get('/health')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b'yes')
+        self.assertEqual(response.data, b"yes")
 
 if __name__ == '__main__':
     unittest.main()
-
