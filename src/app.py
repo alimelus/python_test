f17a797d00205041fe2ca1acfa7a5a258e1be4c8
alimelus <66154377+alimelus@users.noreply.github.com>
2023-10-13 14:03:40 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,11 +1,12 @@
-from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask
+
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
-    return render_template('index.html', date=datetime.now())
-@app.route('/health')
-def alive():
-    return "yes"
-if __name__  == '__main__':
+    return 'Hello, World!'
+
+# ... other route definitions and application configuration ...
+
+if __name__ == '__main__':
     app.run()
